7b7d021c852525d3c512548ab09add72617bfc7d
Simon LUCIDO <simon.lucido@etu.umontpellier.fr>
2021-12-02 21:27:22 +0100

    swagger marche

--- src/main/java/com/example/demo/ControllerTest.java
+++ src/main/java/com/example/demo/ControllerTest.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @RestController
 @RequestMapping("/test")
